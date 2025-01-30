fed99f0d04348c64bdcc15d17f55734824ccde25
prince04kumar <princekumar72131@gmail.com>
2025-01-31 00:44:46 +0530

    first commit

---
+++ test.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+const rooms = {};
+
+io.on('connection', (socket) => {
+    console.log('User connected:', socket.id);
+
+    socket.on('joinRoom', (roomId) => {
+        socket.join(roomId);
+        console.log(`User ${socket.id} joined room ${roomId}`);
+
+        if (!rooms[roomId]) rooms[roomId] = [];
+        rooms[roomId].push(socket.id);
+
+        if (rooms[roomId].length === 2) {
+            io.to(rooms[roomId][0]).emit('createOffer');
+        }
+    });
+
+    socket.on('signal', (data) => {
+        if (data.roomId) {
+            socket.to(data.roomId).emit('signal', data);
+        }
+    });
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected:', socket.id);
+        for (const roomId in rooms) {
+            rooms[roomId] = rooms[roomId].filter(id => id !== socket.id);
+            if (rooms[roomId].length === 0) delete rooms[roomId];
+        }
+    });
+});
+
+server.listen(3000, () => console.log('Server running on http://localhost:3000'));
