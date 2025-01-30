abe3d7f0e6b06ec46de322d5b824a5fb63f4d27d
Prince Kumar <152782679+prince04kumar@users.noreply.github.com>
2025-01-31 00:55:28 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const { Server } = require('socket.io');
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
-
+const PORT = 3000 ;
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
@@ -41,6 +41,6 @@ io.on('connection', (socket) => {
         }
     });
 });
-server.listen(3000, () => {
+server.listen(PORT, () => {
   console.log('Server running on http://localhost:3000');
 });
